bda178105a78ec9504c86c648487a0637cad0858
lesley <lreynolds@roc-dev.com>
2019-10-30 09:58:37 +0100

    Hfst.3

---
+++ src/003/zwemclubs.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $spartelkuikens = 25;
+        $waterbuffels = 32;
+        $Plonsmderin = 11;
+        $Bommetje = 23;
+
+        for($i >= 5; $i++) {
+
+        }
+        echo  "<ul>" . "De spartelkuikens:" . $spartelkuikens . "</ul>" .
+            "<ul>" . "De waterbuffels" . $waterbuffels . "</ul>" .
+            "<ul>" . "Plonsmderin" . $Plonsmderin . "</ul>" .
+            "<ul>" . "Bommetje" . $Bommetje . "</ul>";
+    ?>
+
+</body>
+</html>
+
+
